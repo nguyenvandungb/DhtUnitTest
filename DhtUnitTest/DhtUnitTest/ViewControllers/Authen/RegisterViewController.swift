28b7da55bf4b51aaf0e1b2d9429028c740f33bd0
Nguyen Van Dung <nguyen.van.dungb@framgia.com>
2017-05-16 17:12:04 +0700

    refs add unit test for screen transaction

---
+++ DhtUnitTest/DhtUnitTest/ViewControllers/Authen/RegisterViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegisterViewController.swift
+//  DhtUnitTest
+//
+//  Created by Nguyen Van Dung on 5/13/17.
+//  Copyright © 2017 Dht. All rights reserved.
+//
+
+import Foundation
+
+class RegisterViewController: BaseViewController {
+    
+}
